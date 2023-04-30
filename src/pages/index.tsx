9350b94c4c100f7332c060b8c5f273de93734729
Gayashan Wagachchi <gayashan4lk@gmail.com>
2023-05-01 02:43:39 +0530

    add styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,49 +45,65 @@ export default function Home() {
 	});
 	return (
 		<form onSubmit={handleSubmit}>
-			<div>
-				<label htmlFor='email'>First Name</label>
+			<div className='pl-5 mb-5'>
+				<label htmlFor='email' className='block mb-2 text-sm font-medium'>
+					First Name
+				</label>
 				<input
 					id='firstName'
 					name='firstName'
 					type='text'
 					onChange={handleChange}
 					value={values.firstName}
+					className='border-2 border-gray-300 rounded-md p-2'
 				/>
 				<sub style={{ color: 'red' }}>
 					{errors.firstName ? `${errors.firstName}` : null}
 				</sub>
 			</div>
 
-			<div>
-				<label htmlFor='email'>Last Name</label>
+			<div className='pl-5 mb-5'>
+				<label htmlFor='email' className='block mb-2 text-sm font-medium'>
+					Last Name
+				</label>
 				<input
 					id='lastName'
 					name='lastName'
 					type='text'
 					onChange={handleChange}
 					value={values.lastName}
+					className='border-2 border-gray-300 rounded-md p-2'
 				/>
 				<sub style={{ color: 'red' }}>
 					{errors.lastName ? `${errors.lastName}` : null}
 				</sub>
 			</div>
 
-			<div>
-				<label htmlFor='email'>Email Address</label>
+			<div className='pl-5 mb-5'>
+				<label htmlFor='email' className='block mb-2 text-sm font-medium'>
+					Email Address
+				</label>
 				<input
 					id='email'
 					name='email'
 					type='text'
 					onChange={handleChange}
 					value={values.email}
+					className='border-2 border-gray-300 rounded-md p-2'
 				/>
 				<sub style={{ color: 'red' }}>
 					{errors.email ? `${errors.email}` : null}
 				</sub>
 			</div>
 
-			<button type='submit'>Submit</button>
+			<div className='pl-5 mb-5'>
+				<button
+					type='submit'
+					className='bg-slate-200 border-2 border-slate-300 py-2 px-5 rounded-md font-bold text-slate-800'
+				>
+					Submit
+				</button>
+			</div>
 		</form>
 	);
 }
