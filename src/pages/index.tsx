f3571254b5a35622b9d754d11ea54161e7b735bd
Gayashan Wagachchi <gayashan4lk@gmail.com>
2023-05-01 02:46:13 +0530

    fix styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,66 +44,68 @@ export default function Home() {
 		},
 	});
 	return (
-		<form onSubmit={handleSubmit}>
-			<div className='pl-5 mb-5'>
-				<label htmlFor='email' className='block mb-2 text-sm font-medium'>
-					First Name
-				</label>
-				<input
-					id='firstName'
-					name='firstName'
-					type='text'
-					onChange={handleChange}
-					value={values.firstName}
-					className='border-2 border-gray-300 rounded-md p-2'
-				/>
-				<sub style={{ color: 'red' }}>
-					{errors.firstName ? `${errors.firstName}` : null}
-				</sub>
-			</div>
+		<div className='m-6'>
+			<form onSubmit={handleSubmit}>
+				<div className='ml-5 mb-5'>
+					<label htmlFor='email' className='block mb-2 text-sm font-medium'>
+						First Name
+					</label>
+					<input
+						id='firstName'
+						name='firstName'
+						type='text'
+						onChange={handleChange}
+						value={values.firstName}
+						className='border-2 border-gray-300 rounded-md p-2'
+					/>
+					<sub style={{ color: 'red' }}>
+						{errors.firstName ? `${errors.firstName}` : null}
+					</sub>
+				</div>
 
-			<div className='pl-5 mb-5'>
-				<label htmlFor='email' className='block mb-2 text-sm font-medium'>
-					Last Name
-				</label>
-				<input
-					id='lastName'
-					name='lastName'
-					type='text'
-					onChange={handleChange}
-					value={values.lastName}
-					className='border-2 border-gray-300 rounded-md p-2'
-				/>
-				<sub style={{ color: 'red' }}>
-					{errors.lastName ? `${errors.lastName}` : null}
-				</sub>
-			</div>
+				<div className='ml-5 mb-5'>
+					<label htmlFor='email' className='block mb-2 text-sm font-medium'>
+						Last Name
+					</label>
+					<input
+						id='lastName'
+						name='lastName'
+						type='text'
+						onChange={handleChange}
+						value={values.lastName}
+						className='border-2 border-gray-300 rounded-md p-2'
+					/>
+					<sub style={{ color: 'red' }}>
+						{errors.lastName ? `${errors.lastName}` : null}
+					</sub>
+				</div>
 
-			<div className='pl-5 mb-5'>
-				<label htmlFor='email' className='block mb-2 text-sm font-medium'>
-					Email Address
-				</label>
-				<input
-					id='email'
-					name='email'
-					type='text'
-					onChange={handleChange}
-					value={values.email}
-					className='border-2 border-gray-300 rounded-md p-2'
-				/>
-				<sub style={{ color: 'red' }}>
-					{errors.email ? `${errors.email}` : null}
-				</sub>
-			</div>
+				<div className='ml-5 mb-5'>
+					<label htmlFor='email' className='block mb-2 text-sm font-medium'>
+						Email Address
+					</label>
+					<input
+						id='email'
+						name='email'
+						type='text'
+						onChange={handleChange}
+						value={values.email}
+						className='border-2 border-gray-300 rounded-md p-2'
+					/>
+					<sub style={{ color: 'red' }}>
+						{errors.email ? `${errors.email}` : null}
+					</sub>
+				</div>
 
-			<div className='pl-5 mb-5'>
-				<button
-					type='submit'
-					className='bg-slate-200 border-2 border-slate-300 py-2 px-5 rounded-md font-bold text-slate-800'
-				>
-					Submit
-				</button>
-			</div>
-		</form>
+				<div className='ml-5 mb-5'>
+					<button
+						type='submit'
+						className='bg-slate-200 border-2 border-slate-300 py-2 px-5 rounded-md font-bold text-slate-800'
+					>
+						Submit
+					</button>
+				</div>
+			</form>
+		</div>
 	);
 }
